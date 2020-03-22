70ae776660d3edaa7f3314bff379c08a237dc52a
Pieter Kitslaar <kitslaar@gmail.com>
2020-03-22 13:46:29 +0100

    Updated solution to day 22 to use modular arithmetic

--- 2019/day 22/day_22.py
+++ 2019/day 22/day_22.py
@@ -7,14 +7,39 @@ from tqdm import tqdm
 def factory(x):
     return x
 
+def apply_f(ab, x, N):
+    return (ab['a']*x + ab['b']) % N
+
+def deal_into_new_stack_ab(N):
+    """
+    Factored into f(x) = a*x + b (mod N)
+
+    f(x) = N-1-x
+         = -1*x + (N-1)
+         = -1*x -1 + (N) (mod N)
+         = -1*x -1
+
+    """
+    return dict(a=-1, b=-1)
+
+
 def deal_into_new_stack(N, x):
-    return N-1-x
+    ab = deal_into_new_stack_ab(N)
+    return apply_f(ab, x, N)
 
 def test_deal_into_new_stack():
-    d = factory(10)
     deal_10 = partial(deal_into_new_stack, 10)
-    d2 = map(deal_10, range(10))
+    d2 = deal_using_f(deal_10, range(10))
     assert(list(d2) == [9, 8, 7, 6, 5, 4, 3, 2, 1, 0])
+
+def cut_N_cards_ab(N, cut):
+    """
+    Factored into f(x) = a*x + b (mod N)
+
+    f(x) = x-cut (mod N)
+         = 1*x - cut
+    """
+    return dict(a=1, b=-cut)
 
 def cut_N_cards(N, cut, x):
     """
@@ -22,20 +47,28 @@ def cut_N_cards(N, cut, x):
     [3, 4, 5, 6, 7, 8, 9, 0, 1, 2] cut = 3
     [6, 7, 8, 9, 0, 1, 2, 3, 4, 5] cut =-4
     """
-    if cut > 0:
-        return (x+cut) % N
-    else:
-        return (x+(N+cut)) % N
+    ab = cut_N_cards_ab(N,cut)
+    return apply_f(ab, x, N)
 
 def test_cut_N_cards():
     cut_10_3 = partial(cut_N_cards, 10, 3)
-    d2 = map(cut_10_3, range(10))
+    d2 = deal_using_f(cut_10_3, range(10))
     assert(list(d2) == [3, 4, 5, 6, 7, 8, 9, 0, 1, 2])
 
 def test_cut_N_cards_negative():
     cut_10_n4 = partial(cut_N_cards, 10, -4)
-    d2 = map(cut_10_n4, range(10))
+    d2 = deal_using_f(cut_10_n4, range(10))
     assert(list(d2) == [6, 7, 8, 9, 0, 1, 2, 3, 4, 5])
+
+def test_cut_7_cards_negative_3():
+    cut_f = partial(cut_N_cards, 7, -3)
+    d2 = deal_using_f(cut_f, range(7))
+    assert(list(d2) ==  [4, 5, 6, 0, 1, 2, 3])
+
+def test_cut_7_cards_n_3():
+    cut_f = partial(cut_N_cards, 7, 3)
+    d2 = deal_using_f(cut_f, range(7))
+    assert(list(d2) ==  [3, 4, 5, 6, 0, 1, 2])
 
 increment_brute_cache = {0:0}
 def deal_increment_brute(N, increment, x):
@@ -63,6 +96,15 @@ def deal_increment_fast(N, increment, x):
                 cache[x] = j
                 return j
             n+=1
+
+def deal_increment_ab(N, increment):
+    """
+    Factor into f(x) = a*x + b (mod N)
+
+    f(x) = increment*x     (mod N)
+         = increment*x + 0 (mod N)
+    """
+    return dict(a=increment, b=0)
 
 def deal_increment(N, increment, x):
     """
@@ -182,8 +224,9 @@ deal into new stack
 """
 
 def test_example1():
-    d = solve(example1, 10)
-    assert(d == [0, 3, 6, 9, 2, 5, 8, 1, 4, 7])
+    for s_func in (solve_explicit, solve_analytic):
+        d = s_func(example1, 10)
+        assert(d == [0, 3, 6, 9, 2, 5, 8, 1, 4, 7])
 
 example2="""\
 cut 6
@@ -192,9 +235,10 @@ deal into new stack
 """
 #Result: 3 0 7 4 1 8 5 2 9 6
 def test_example2():
-    d = solve(example2, 10)
-    assert(d == [3, 0, 7, 4, 1, 8, 5, 2, 9, 6])
-
+    for s_func in (solve_explicit, solve_analytic):
+        d = s_func(example2, 10)
+        assert(d == [3, 0, 7, 4, 1, 8, 5, 2, 9, 6])
+    
 example3="""\
 deal with increment 7
 deal with increment 9
@@ -202,8 +246,9 @@ cut -2
 """
 #Result: 6 3 0 7 4 1 8 5 2 9
 def test_example3():
-    d = solve(example3, 10)
-    assert(d == [6, 3, 0, 7, 4, 1, 8, 5, 2, 9])
+    for s_func in (solve_explicit, solve_analytic):
+        d = s_func(example3, 10)
+        assert(d == [6, 3, 0, 7, 4, 1, 8, 5, 2, 9])
 
 example4="""\
 deal into new stack
@@ -219,70 +264,66 @@ cut -1
 """
 #Result: 9 2 5 8 1 4 7 0 3 6
 def test_example4():
-    d = solve(example4, 10)
-    assert(d == [9, 2, 5, 8, 1, 4, 7, 0, 3, 6])
+    for s_func in (solve_explicit, solve_analytic):
+        d = s_func(example4, 10)
+        assert(d == [9, 2, 5, 8, 1, 4, 7, 0, 3, 6])
 
 def part1():
     with open(Path(__file__).parent / 'input.txt', 'r') as f:
         main_input = f.read()
-    d = solve(main_input, 10007)
-    sol_part1 = d.index(2019)
-    print('Part 1:', sol_part1)
-    assert(7171 == sol_part1)
+    if False:
+        # Explicit
+        d = solve_explicit(main_input, 10007)
+        sol_part1 = d.index(2019)
+        print('Part 1:', sol_part1)
+        assert(7171 == sol_part1)
 
-def test_reverse_lookup_example4():
-    #assert(d == [9, 2, 5, 8, 1, 4, 7, 0, 3, 6])
-    funcs = list(parse_funcs(example4, 10))
-    solutions = [list(range(10))]
-    for i in range(100):
-        solutions.append(list(solve2(funcs, solutions[-1])))
+    # Analytic
+    ab = solve_analytic_ab(main_input, 10007)
+    sol_part1_analytic = apply_f(ab, 2019, 10007)
+    print('Part 1 (analytic):', sol_part1_analytic)
+    assert(7171 == sol_part1_analytic)
 
-    for i in range(10):
-        assert((solutions[1][i],) == reverse_lookup(funcs, 1, i))
-        assert((solutions[2][i],) == reverse_lookup(funcs, 2, i))
-        assert((solutions[4][i],) == reverse_lookup(funcs, 4, i))
-        assert((solutions[5][i],) == reverse_lookup(funcs, 5, i))
-        assert((solutions[9][i],) == reverse_lookup(funcs, 9, i))
-        assert((solutions[99][i],) == reverse_lookup(funcs, 99, i))
+def pow_compose(f, k, N):
+    g = dict(a=1, b=0)
+    while k > 0:
+        if k % 2 != 0:
+            g = compose(g, f, N)
+        k = k//2
+        f = compose(f, f, N)
+    return g
 
 
 def part2():
     with open(Path(__file__).parent / 'input.txt', 'r') as f:
         main_input = f.read()
-    funcs = list(parse_funcs(main_input, 119315717514047))
-    num_repeats = 101741582076661
-    sol = reverse_lookup(funcs, num_repeats, target_idx=2020)
+    N = 119315717514047
+    ab = solve_analytic_ab(main_input, N)
+
+    # Now we have to resort to pure algebra to find the solution
+    # to applying ab, num_repeats times
+    # I took this from: https://codeforces.com/blog/entry/72593
+    k = num_repeats = 101741582076661
+
+    # This came from https://topaz.github.io/paste/#XQAAAQAgBQAAAAAAAAAzHIoib6pENkSmUIKIED8dy140D1lKWSMhNhZz+hjKgIgfJKPuwdqIBP14lxcYH/qI+6TyUGZUnsGhS4MQYaEtf9B1X3qIIO2JSejFjoJr8N1aCyeeRSnm53tWsBtER8F61O2YFrnp7zwG7y303D8WR4V0eGFqtDhF/vcF1cQdZLdxi/WhfyXZuWC+hs8WQCBmEtuId6/G0PeMA1Fr78xXt96Um/CIiLCievFE2XuRMAcBDB5We73jvDO95Cjg0CF2xgF4yt3v4RB9hmxa+gmt6t7wRI4vUIGoD8kX2k65BtmhZ7zSZk1Hh5p1obGZ6nuuFIHS7FpuSuv1faQW/FuXlcVmhJipxi37mvPNnroYrDM3PFeMw/2THdpUwlNQj0EDsslC7eSncZQPVBhPAHfYojh/LlqSf4DrfsM926hSS9Fdjarb9xBYjByQpAxLDcmDCMRFH5hkmLYTYDVguXbOCHcY+TFbl+G/37emZRFh/d+SkeGqbFSf64HJToM2I7N2zMrWP7NDDY5FWehD5gzKsJpEg34+sG7x2O82wO39qBlYHcYg1Gz4cLBrH1K1P+KWvEdcdj/NBtrl6yftMlCu6pH4WTGUe9oidaiRuQZOGtw71QsTQUuhpdoWO4mEH0U9+CiPZCZLaQolFDSky1J9nDhZZHy3+ETcUeDOfSu+HI3WuKC0AtIRPdG8B9GhtxZQKAx+5kyi/ek7A2JAY9SjrTuvRADxx5AikbHWXIsegZQkupAc2msammSkwY8dRMk0ilf5vh6kR0jHNbSi0g0KJLCJfqggeX24fKk5Mdh8ULZXnMfMZOmwEGfegByYbu91faLijfW4hoXCB1nlsWTPZEw2PCZqqhl9oc1q25H2YkkvKLxEZWl6a9eFuRzxhB840I1zdBjUVgfKd9/V4VdodzU2Z2e+VEh7RbJjQNFC/rG8dg==
+    # Fermat's little theorem gives a simple inv:
+    def inv(a, n): return pow(a, n-2, n)
+
+    a, b = ab['a'], ab['b']
+    A =  pow(a, k, N)
+    B = (b*(A-1)*inv(a-1, N)) % N
+    AB = {'a': A, 'b':B}
+    
+    # Now we want to find the reverse so we can see which
+    # card ends up at position 2020
+    # If f(x) = a*x + b
+    # than the inverse f-1(x)
+    # is f-1(x) = (x - B)/A
+    sol = ((2020 - B)*inv(A,N)) % N
     print('Part 2:', sol)
+    assert(73394009116480 == sol)
 
-def reverse_lookup(funcs, num_repeats, target_idx):
-    d = (target_idx,)
-    cache = {}
-    repeat_i = -1
-    values = []
-    for i in tqdm(range(num_repeats)):
-        try:
-            new_d = cache[d]
-            repeat_i = i
-            print('repeat after', i, 'shuffles')
-            break
-        except KeyError:
-            new_d = d
-            for f in funcs:
-                new_d = tuple(map(f, new_d))
-            cache[d] = new_d
-        #print(d, new_d)
-        d = new_d
-        values.append(d)
-
-    if repeat_i > -1:
-        num_remaining = num_repeats % (repeat_i)
-        d = (target_idx,)
-        for i in range(num_remaining):
-            d = cache[d]
-        return d
-    return d
 
 if __name__ == "__main__":
-    #test_deal_inc_11_3()
     part1()
     part2()
