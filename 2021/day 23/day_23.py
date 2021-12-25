812c77129056ca90be74ce2c3ba8c3e1ee66132e
Pieter Kitslaar <kitslaar@gmail.com>
2021-12-25 17:06:35 +0100

    Solution to AoC Day 23 Part 2

--- 2021/day 23/day_23.py
+++ 2021/day 23/day_23.py
@@ -45,116 +45,146 @@ T_ENERGY={'A':1, 'B': 10, 'C': 100, 'D': 1000}
 
 
 class State:
-    def __init__(self, burrow, energy=0):
+    def __init__(self, burrow, energy=0, y_max = None, room_rows=None):
         self.burrow = burrow
+        self.y_max = y_max or max(p[0] for p in self.burrow)
+        self.room_rows = room_rows or [*range(2,self.y_max+1)]
         self.energy = energy
-        self.theoretical_best_energy = 0
-        self.update_min_energy_to_done()
 
     def clone(self):
-        return State(self.burrow.copy(), self.energy)
+        return State(self.burrow.copy(), energy=self.energy, y_max=self.y_max, room_rows=self.room_rows)
 
     def plot(self):
-        y_max = max(p[0] for p in self.burrow)
         x_max = max(p[1] for p in self.burrow)
-        for y in range(y_max+1):
+        for y in range(self.y_max+1):
             for x in range(x_max+1):
                 print(self.burrow.get((y,x), ' '), sep='',end='')
             print()
 
     def is_done(self):
         for t, col in ROOMS.items():
-            if self.burrow[(2,col)] != t:
-                return False
+            for row in range(2, self.y_max+1):
+                if self.burrow[(row,col)] != t:
+                    return False
         return True
-
-    def update_min_energy_to_done(self):
-        min_energy = 0
-        pos_per_t = {}
-        amphipods = [(p,v) for p,v in self.burrow.items() if v !='.']
-        for p,t in amphipods:
-            pos_per_t.setdefault(t,[]).append(p)
-        for t, p in pos_per_t.items():
-            required_col = ROOMS[t]
-            distances = [(dist(p[i],(r,required_col)),i,r) for i in (0,1) for r in (2,3)]
-            distances.sort(key = lambda t: t[0])
-            closest = distances[0]
-            closest_d, c_i, c_r  = closest
-            other_d = [d for d in distances if d[1]!=c_i and d[2]!=c_r][0][0]
-            min_energy += closest_d*T_ENERGY[t]
-            min_energy += other_d*T_ENERGY[t]
-        self.theoretical_best_energy = self.energy + min_energy
 
     def move_options(self):
         amphipods = [(p,v) for p,v in self.burrow.items() if v !='.']
-        all_options = {}
+        all_options = []
         for pos, t in amphipods:
-            options = all_options[(pos,t)] = []
+            t_room = ROOMS[t]
 
-            if pos[0] in (2,3):
-                # in a room, lets find an option in the hall way
-                in_wrong_room = pos[1] != ROOMS[t] 
+            if pos[1] == t_room and pos[0]>1 and all(self.burrow[(r,t_room)]==t for r in range(pos[0]+1,self.y_max+1)):
+                    # amphi already on correct location
+                    continue
+            
+            if True:
+                visited = set()
+                front = [pos]
+                visited.add(pos)
 
-                # let's find if we can move out of the room
-                can_move_out = False
-                if pos[0] == 3 and in_wrong_room:
-                    # lowest position and we are in the wrong room
-                    if self.burrow[(2,pos[1])] == '.':
-                        # position above is empty
-                        can_move_out=True
-                elif pos[0] == 2:
-                    neighbor_type = self.burrow[(3,pos[1])]
-                    neighbor_in_wrong_room = ROOMS[neighbor_type] != pos[1]
-                    if in_wrong_room or neighbor_in_wrong_room:
-                        can_move_out = True
+                while front:
+                    current = front.pop()
+                    N = [(0,-1),(0,1),(-1,0)] # left, right and up always allowed
+                    if current[1] == t_room:
+                        N.append((1,0)) # allow to move down in own room
+                    for n in N:
+                        n_pos = (current[0]+n[0],current[1]+n[1])
+                        if n_pos not in visited:
+                            n_v = self.burrow.get(n_pos)
+                            if n_v == '.':
+                                front.append(n_pos)
+                                visited.add(n_pos)
+                visited.remove(pos)
+
+                if pos[0] > 1: # only give hall way positions as option when currently in a room
+                    all_options.extend([((pos,t),p) for p in visited if p[0]==1 and (p[1] not in (3,5,7,9))])
+
+                if pos[0] == 1:
+                    # check positions ending up in target room
+                    t_room_pos = [p for p in visited if p[1]==t_room and p[0]!=1]
+                    if t_room_pos:
+                        t_room_pos.sort() # sort based on row and take the highest
+                        best_t_room_pos = t_room_pos[-1]
+                        
+                        allowed = True
+                        for r in range(best_t_room_pos[0]+1,self.y_max+1):
+                            if self.burrow[(r,best_t_room_pos[1])] != t:
+                                allowed = False
+                        
+                        if allowed:
+                            # found best end position in room
+                            return [((pos, t), best_t_room_pos)]
+                            #all_options.append((pos,best_t_room_pos))
+
+
+            if False:
+                if pos[0] in self.room_rows:
+                    # check if the space above is empty
+                    above_empty = True
+                    for r in range(2,pos[0]):
+                        if self.burrow[(r,pos[1])] != '.':
+                            above_empty = False
+                            break
+
+                    # let's find if we can move out of the room
+                    can_move_out = False
+                    if above_empty:
+                        for r in range(pos[0],self.y_max+1):
+                            n_t = self.burrow[(r,pos[1])]
+                            if  n_t != '.' and ROOMS[n_t] != pos[1]:
+                                # somebody is in the wrong room
+                                can_move_out = True
+                                break
+
+                    if can_move_out:
+                        # we can move out of the room
+                        # let's see where we can move to in the hall way
+
+                        # search to right
+                        for x in range(pos[1],11+1):
+                            if self.burrow[(1,x)] != '.':
+                                break
+                            elif x not in (3,5,7,9):
+                                options.append((1,x))
+
+                        # search to left
+                        for x in range(pos[1],0,-1):
+                            if self.burrow[(1,x)] != '.':
+                                break
+                            elif x not in (3,5,7,9):
+                                options.append((1,x))
                 
-                if can_move_out:
-                    # we can move out of the room
-                    # let's see where we can move to in the hall way
+                else:
+                    # in the hall way
+                    # only option is to move to its own room
+                    target_col = ROOMS[t]
 
-                    # search to right
-                    for x in range(pos[1],11+1):
-                        if self.burrow[(1,x)] != '.':
+                    target_row = None
+                    for row in range(self.y_max, 1, -1):
+                        v_row = self.burrow[(row,target_col)]
+                        if v_row == '.':
+                            target_row = row
                             break
-                        elif x not in (3,5,7,9):
-                            options.append((1,x))
+                        elif v_row != t:
+                            # row does not contain same types
+                            break
 
-                    # search to left
-                    for x in range(pos[1],0,-1):
-                        if self.burrow[(1,x)] != '.':
-                            break
-                        elif x not in (3,5,7,9):
-                            options.append((1,x))
-            else:
-                # in the hall way
-                # only option is to move to its own room
-                target_col = ROOMS[t]
-                target_row = None
-
-                # find target room position
-                upper_pos = (2,target_col)
-                upper_v = self.burrow[upper_pos]
-                lower_pos = (3,target_col)
-                lower_v = self.burrow[lower_pos]
-                if upper_v == '.':
-                    if lower_v == '.':
-                        target_row=3
-                    elif lower_v == t:
-                        target_row=2
-                
-                # if we have an empty slot in our room
-                # we need to know if we can reach it
-                if target_row is not None:
-                    direction = [-1,1][target_col > pos[1]]
-                    can_reach = True
-                    for x in range(pos[1]+direction,target_row+direction,direction):
-                        if self.burrow[(1,x)] != '.':
-                            can_reach = False
-                            break
-                    if can_reach:
-                        # no break we are able to reach the room
-                        options.append((target_row, target_col))
-        return {k:v for k,v  in all_options.items() if v}
+                    for t_pos in [pos]:
+                        # if we have an empty slot in our room
+                        # we need to know if we can reach it
+                        if target_row is not None:
+                            direction = [-1,1][target_col > t_pos[1]]
+                            can_reach = True
+                            for x in range(t_pos[1]+direction,target_row+direction,direction):
+                                if self.burrow[(1,x)] != '.':
+                                    can_reach = False
+                                    break
+                            if can_reach:
+                                # no break we are able to reach the room
+                                options.append((target_row, target_col))
+        #return {k:v for k,v  in all_options.items() if v}
+        return all_options
 
 
 
@@ -166,17 +196,38 @@ def parse(txt):
                 burrow[(y,x)]=v
     return State(burrow)
 
+def plot_options(state):
+    for amp, options in state.move_options().items():
+        print(amp)
+        c= state.clone()
+        c.burrow[amp[0]] = '*'
+        for op in options:
+            c.burrow[op] = amp[1].lower()
+        c.plot()
+        print()
+        print()
+
+done_1="""\
+#############
+#.......D...#
+###A#B#C#.###
+  #A#B#C#D#
+  #########"""
+
 def test_state():
     e = parse(example)
     assert e.is_done() == False
-    
+    #plot_options(e)
+
     d = parse(done)
+    plot_options(d)
     assert d.is_done() == True
-    d_options = d.move_options()
-    assert len(d_options) == 0
+    #d_options = d.move_options()
+    #assert len(d_options) == 0
+
+import heapq
 
 def solve(state: State):
-    state.update_min_energy_to_done()
     best_solution = None
     states  = [state]
     while states:
@@ -185,27 +236,27 @@ def solve(state: State):
         if best_solution and s.energy > best_solution.energy:
             continue
         s_options = s.move_options()
-        for (amp_pos, amp_t), options in s_options.items():
-            for op in options:
+        for (amp_pos, amp_t), op in s_options:
+            #for op in options:
                 num_steps = abs(op[0]-amp_pos[0])+abs(op[1]-amp_pos[1])
                 added_energy = num_steps*T_ENERGY[amp_t]
-                new_s : State = s.clone()
-                new_s.burrow[amp_pos]='.'
-                new_s.burrow[op] = amp_t
-                new_s.energy += added_energy
-                new_s.update_min_energy_to_done()
-                if not best_solution or (new_s.theoretical_best_energy < best_solution.energy):
+               
+                if not best_solution or ((s.energy+added_energy) < best_solution.energy):
+                    new_s : State = s.clone()
+                    new_s.burrow[amp_pos]='.'
+                    new_s.burrow[op] = amp_t
+                    new_s.energy += added_energy
+
                     if new_s.is_done():
-                        print('found solution', new_s.energy)
+                        print('found solution', new_s.energy, len(states))
                         best_solution = new_s
-                        #s.plot()
                     else:
                         new_states.append(new_s)
 
         states.extend(new_states)
-        states.sort(key=lambda s: s.theoretical_best_energy)
+        states.sort(key=lambda s: s.energy)
         if best_solution:
-            states = [s for s in states if s.theoretical_best_energy < best_solution.energy]
+            states = [s for s in states if s.energy < best_solution.energy]
         #print(len(states))
     return best_solution.energy
 
@@ -224,13 +275,10 @@ example_b="""\
   #A#B#C#A#
   #########"""
 def test_example():
-    e= parse(example)
-    #e.energy = 3510#+2003
-    solve(e)
+    result = solve(parse(example))
+    print(result)
+    assert 12521 == result
 
-    #solve(parse(example_1))
-
-    #solve(parse(example_2))
 
 def test_part1():
     result = solve(parse(get_input()))
@@ -238,7 +286,39 @@ def test_part1():
     assert 14546 == result
 
 
+example_part_2="""\
+#############
+#...........#
+###B#C#B#D###
+  #D#C#B#A#
+  #D#B#A#C#
+  #A#D#C#A#
+  #########"""
+
+def test_example_part2():
+    result = solve(parse(example_part_2))
+    print(result)
+    assert 12521 == result
+
+part2 = """\
+#############
+#...........#
+###D#A#D#C###
+  #D#C#B#A#
+  #D#B#A#C#
+  #C#A#B#B#
+  #########"""
+
+def test_part2():
+    result = solve(parse(part2))
+    print('Part 2', result)
+    assert 42308 == result
+
+
 if __name__ == "__main__":
     #test_state()
+    #print('example')
     #test_example()
-    test_part1()
+    #test_part1()
+    #test_example_part2()
+    test_part2()
