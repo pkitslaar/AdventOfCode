cba309f953229fb3ea1c9ca7480f0f71132dd386
Pieter Kitslaar <pkitslaar@medisimaging.com>
2022-11-25 09:01:46 +0100

    Solution to 2018 - Day 23 Part 2 - with little help from https://www.reddit.com/r/adventofcode/comments/a8s17l/comment/ecfmpy0/

--- 2018/day 23/solution.py
+++ 2018/day 23/solution.py
@@ -3,7 +3,8 @@
 #
 # Pieter Kitslaar
 #
-
+from pathlib import Path
+THIS_DIR = Path(__file__).parent
 import numpy as np
 import re
 regex = re.compile('-*\d+')
@@ -31,76 +32,24 @@ def parse(data):
     points.sort(key = lambda t: t[-1], reverse = True) # sort by radius
     return np.array(points, dtype=np.int_)
 
-def expand(k, data):
-    expanded = []
-    to_process = [[k]]
-    while to_process:
-        expanded.extend(to_process)
-        new_to_process = []
-        for sequence in to_process:
-            root, c = sequence[0], sequence[-1]
-            root_parents = data.get(root, set())
-            c_parents = data.get(c, set())
-            if c_parents:
-                for p in c_parents:
-                    if p in root_parents:
-                        new_to_process.append(sequence + [p])
-        to_process = new_to_process
-    expanded.extend(to_process)
-    print('Returning largest')
-    expanded.sort(key = lambda g: len(g), reverse=True)
-    return expanded[0]
-
-def overlap(points):
-    print('Computing overlap')
-    overlaps = []
-    for i, p in enumerate(points):
-        #other_points = points[i+1:]
-        has_overlap = np.sum(np.abs(points[:, COORDS] - p[COORDS]), axis=1) <= (points[:, R] + p[R])
-        overlappig = np.argwhere(has_overlap).flatten().tolist()
-        overlap_set = set(overlappig)
-        overlaps.append(overlap_set)
-    #print(overlaps)
-    overlap_keys = set([tuple(ol) for ol in overlaps])
-    overlap_count = {}
-    for ol_key in overlap_keys:
-        ol = set(ol_key)
-        count = 0
-        for other in overlaps:
-            if ol.issubset(other):
-                count += 1
-        overlap_count[ol_key] = count
-    #print(overlap_count)
-    full_overlaps = [(v,k) for k,v in overlap_count.items() if v == len(k)]
-    full_overlaps.sort(reverse=True)
-
-    # find the groups with maximum overlap
-    max_overlaps = [full_overlaps[0]]
-    for n, group in full_overlaps[1:]:
-        if n == max_overlaps[0][0]:
-            max_overlaps.append((n,group))
-    print('Found', len(max_overlaps), 'overlap groups')
-    #print(*max_overlaps, sep='\n')
-    for n, g in max_overlaps:
-        g_points = np.take(points, g, axis=0)
-        dist_origin = np.sum(np.abs(g_points[:, COORDS]), axis=1) - g_points[:, R]
-        print(dist_origin)
-        min_distance_all_overlap = np.max(dist_origin)
-        print('PART 2: distance max overlap', min_distance_all_overlap)
-        1/0
-
 def solve(data):
     points = parse(data)
-    largest_point = points[np.argmax(points[:,3], axis=0)]
-    #print(largest_point)
+    largest_point = points[np.argmax(points[:,3], axis=0)]    
     distance = np.sum(np.abs(points[:,0:3] - largest_point[0:3]), axis=1)
     in_range = (largest_point[3] - distance) >= 0
-    print('PART 1: num in range', np.sum(in_range))
-    return points
+    return np.sum(in_range)
+    
+def test_example():    
+    in_range = solve(example_data)
+    print('Example 1:', in_range)
+    assert(in_range == 7)
+    
 
-print('Example')
-p = solve(example_data)
-print()
+def test_part1():
+    with open(THIS_DIR / 'input.txt') as f:
+        in_range = solve(f.read())
+        print('PART 1:', in_range)
+        assert(in_range == 943)
 
 example_part_2 = """\
 pos=<10,12,12>, r=2
@@ -110,13 +59,171 @@ pos=<14,14,14>, r=6
 pos=<50,50,50>, r=200
 pos=<10,10,10>, r=5
 """
-p = solve(example_part_2)
-overlap(p)
 
-if True:
-    print('Assignment')
-    with open('input.txt') as f:
-        p = solve(f.read())
-        overlap(p)
-        print()
+class Box:
+    def __init__(self, min_x, max_x, min_y, max_y, min_z, max_z):        
+        self.bounds = tuple([
+            min_x,
+            max_x,
+            min_y,
+            max_y,
+            min_z,
+            max_z,
+        ])
 
+    def __eq__(self, __o: object) -> bool:
+        return self.bounds == __o.bounds
+
+    def __lt__(self, other):
+        return self.bounds < other.bounds
+
+    def __hash__(self) -> int:
+        return hash(self.bounds)
+
+    def __repr__(self):
+        return f"Box(*{self.bounds})"
+
+    def get_bounds_for_dim(self, dim):
+        return self.bounds[2*dim], self.bounds[(2*dim) + 1]
+
+    def intersects(self, bot):
+        # This implementation was taken from 
+        # https://www.reddit.com/r/adventofcode/comments/a8s17l/comment/ecfmpy0/
+        d = 0
+        for dim in range(3):
+            min_d, max_d = self.get_bounds_for_dim(dim)
+            d += abs(bot[dim] - min_d) + abs(bot[dim] - max_d)
+            d -= max_d - min_d
+        d //= 2            
+        return d <= bot[3]
+
+    def single_position(self):
+        return self.bounds[0] == self.bounds[1] and self.bounds[2] == self.bounds[3] and self.bounds[4] == self.bounds[5]
+
+    def center(self):
+        center = []
+        for dim in range(3):
+            min_d, max_d = self.get_bounds_for_dim(dim)
+            split_d = min_d + (max_d - min_d) // 2
+            center.append(split_d)
+        return center        
+
+    def split_dim(self, dim):
+        min_d, max_d = self.get_bounds_for_dim(dim)
+        split_d = min_d + (max_d - min_d) // 2
+        if split_d != max_d:
+            return [(min_d, split_d), (split_d+1, max_d)]
+        return [(min_d, max_d)]
+    
+    def partition(self):        
+        for new_x_bounds in self.split_dim(0):
+            for new_y_bounds in self.split_dim(1):
+                for new_z_bounds in self.split_dim(2):
+                    yield Box(
+                        new_x_bounds[0], new_x_bounds[1], 
+                        new_y_bounds[0], new_y_bounds[1], 
+                        new_z_bounds[0], new_z_bounds[1])
+        
+def test_box_partition():
+    a = Box(0, 10, 0, 10, 0, 10)
+    p_a = list(a.partition())
+    assert(len(p_a) == 8)    
+    assert(p_a[0] == Box(0, 5, 0, 5, 0, 5))
+    assert(p_a[1] == Box(0, 5, 0, 5, 6, 10))
+    assert(p_a[2] == Box(0, 5, 6, 10, 0, 5))
+    assert(p_a[3] == Box(0, 5, 6, 10, 6, 10))
+    assert(p_a[4] == Box(6, 10, 0, 5, 0, 5))
+    assert(p_a[5] == Box(6, 10, 0, 5, 6, 10))
+    assert(p_a[6] == Box(6, 10, 6, 10, 0, 5))
+    assert(p_a[7] == Box(6, 10, 6, 10, 6, 10))
+
+    a = Box(0, 1, 0, 1, 0, 1)
+    p_a = list(a.partition())    
+    assert(len(p_a) == 8)    
+    assert(p_a[0] == Box(0, 0, 0, 0, 0, 0))    
+    assert(p_a[1] == Box(0, 0, 0, 0, 1, 1))    
+    assert(p_a[2] == Box(0, 0, 1, 1, 0, 0))    
+    assert(p_a[3] == Box(0, 0, 1, 1, 1, 1))
+    assert(p_a[4] == Box(1, 1, 0, 0, 0, 0))    
+    assert(p_a[5] == Box(1, 1, 0, 0, 1, 1))    
+    assert(p_a[6] == Box(1, 1, 1, 1, 0, 0))    
+    assert(p_a[7] == Box(1, 1, 1, 1, 1, 1))        
+
+    a = Box(0, 0, 0, 0, 0, 2)
+    p_a = list(a.partition())
+    assert(len(p_a) == 2)    
+    assert(p_a[0] == Box(0, 0, 0, 0, 0, 1))
+    assert(p_a[1] == Box(0, 0, 0, 0, 2, 2)) 
+
+    a = Box(0, 0, 0, 0, 0, 0)
+    p_a = list(a.partition())
+    assert(len(p_a) == 1)    
+    assert(p_a[0] == Box(0, 0, 0, 0, 0, 0))
+
+import heapq
+
+def solve2(d):
+    points = parse(d)
+    
+    min_total = np.min(points - points[:, R][:,np.newaxis], axis=0)
+    max_total = np.max(points + points[:, R][:,np.newaxis], axis=0)    
+
+    def compute_intersections(b):
+        return sum(1 for nb in points if b.intersects(nb))
+
+    def compute_cost(b):
+        intersections = compute_intersections(b)
+        
+        box_radius_values = []
+        for dim in range(3):
+            min_d, max_d = b.get_bounds_for_dim(dim)
+            box_radius_values.append(max_d - min_d)
+        box_radius = min(box_radius_values)
+
+        dist_center = sum(np.abs(b.center()))
+
+        return (-intersections, -box_radius, dist_center )
+
+    big_box = Box(
+        min_total[0], max_total[0],
+        min_total[1], max_total[1],
+        min_total[2], max_total[2],
+    )
+    
+    best_single = None
+    
+    test_boxes = [(compute_cost(big_box), big_box)]
+    heapq.heapify(test_boxes)       
+    while test_boxes:                
+        (this_cost, tb) = heapq.heappop(test_boxes)              
+        #print(len(test_boxes), this_cost, tb)
+        if tb.single_position():                    
+            if not best_single or this_cost < best_single[0]:
+                best_single = (this_cost, tb)
+                #print('best single pos', best_single)                
+                break
+        else:
+            for new_tb in tb.partition():                
+                cost = compute_cost(new_tb)                                 
+                heapq.heappush(test_boxes, (cost, new_tb))
+    return best_single[0][-1]
+    
+
+def test_example2():    
+    dist = solve2(example_part_2)
+    print('Example 2:', dist)
+    assert(dist == 36)
+
+def test_part2():
+    with open(THIS_DIR / 'input.txt') as f:
+        d = f.read()        
+        dist = solve2(d)
+        print('PART 2:', dist)
+        assert(dist == 84087816)
+
+if __name__ == "__main__":    
+    test_box_partition()
+    test_example()
+    test_part1()
+    test_example2()
+    test_part2()
