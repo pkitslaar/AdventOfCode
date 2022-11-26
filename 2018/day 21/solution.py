c1e32a23915b9ab70ec09c5129f0df71346e6ad7
Pieter Kitslaar <pkitslaar@medisimaging.com>
2022-11-26 11:29:41 +0100

    Work on 2018 - Day 21

--- 2018/day 21/solution.py
+++ 2018/day 21/solution.py
@@ -4,7 +4,8 @@
 #
 # Pieter Kitslaar
 #
-
+from pathlib import Path
+THIS_DIR = Path(__file__).parent
 from collections import defaultdict
 
 class CPU(object):
@@ -83,7 +84,7 @@ class CPU(object):
         return [self.registers[i] for i in range(highest_register+1)]
 
 
-def execute_program(txt_data, initial_data = [0]*6):
+def execute_program(txt_data, initial_data = [0]*6, cb = None):
     ip_adress = None
     instructions = []
     for l in txt_data.splitlines():
@@ -101,22 +102,81 @@ def execute_program(txt_data, initial_data = [0]*6):
     while True:
         if ip >= len(instructions):
             break
+        if cb:
+            cb(ip, cpu)
         op_name, call_args = instructions[ip]
         cpu.set_r(ip_adress, ip)
         before = cpu.dump()
         cpu.call(op_name, *call_args)
         num_instructions += 1
         after = cpu.dump()
-        print(f"ip={ip} {before} {op_name} {call_args} {after}")
+        #print(f"ip={ip} {before} {op_name} {call_args} {after}")
         ip = cpu.get_r(ip_adress) + 1
     return cpu.dump(), num_instructions
 
-with open('input.txt') as f:
-    data = f.read()
+def fast_execute(initial_data = [0]*6):
+    r = initial_data[:]
 
-# PART 1
-print(execute_program(data, [22411,0,0,0]))
+    # ip=5
+    r[3] = 0
+
+    while True:
+        
+        # ip = 6, 7
+        r[5] = r[3] | 65536
+        r[3] = 15028787
+
+        while True:
+            # update r[3]
+            # ip=8 - 12
+            r[2] = r[5] & 255
+            r[3] = r[3] + r[2]
+            r[3] = r[3] & 16777215
+            r[3] = r[3] * 65899
+            r[3] = r[3] & 16777215
+
+            if r[5] >= 256:
+                # ip-17
+                r[2] = 0
+
+                # update r[4]
+                while True:
+                    r[4] = r[2] + 1
+                    r[4] = r[4]*256
+
+                    if r[4] > r[5]:
+                        break
+                    else:
+                        r[2] = r[2] + 1
+                
+                # r[4] > r[5]
+                r[5] = r[2] # jump back to ip=8
+            else:    
+                # ip=26
+                if r[3] == r[0]:
+                    return r # halt
 
 
+def test_part1():
+    with open(THIS_DIR / 'input.txt') as f:
+        data = f.read()
 
+    # PART 1
+    print(execute_program(data, [13270004,0,0,0,0,0]))
+    fast_execute([13270004,0,0,0,0,0])
+
+def test_part2():
+    with open(THIS_DIR / 'input.txt') as f:
+        data = f.read()
+
+    # PART 2
+    r3_values = []
+    def check_ip_28(ip, cpu):
+        if ip == 8 or ip==9:
+            print(ip, cpu.dump())
+    print(execute_program(data, [13181393], cb = check_ip_28))
+
+if __name__ == "__main__":
+    test_part1()
+    #test_part2()
 
