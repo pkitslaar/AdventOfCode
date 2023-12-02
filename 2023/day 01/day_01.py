985de21fe642bb3ec89808ef6111685d596b89b5
Pieter Kitslaar <pkitslaar@medisimaging.com>
2023-12-02 09:19:17 +0100

    black formatting

--- 2023/day 01/day_01.py
+++ 2023/day 01/day_01.py
@@ -4,11 +4,14 @@ Pieter Kitslaar
 """
 
 from pathlib import Path
+
 THIS_DIR = Path(__file__).parent
 
+
 def data():
-    with open(THIS_DIR / 'input.txt') as f:
+    with open(THIS_DIR / "input.txt") as f:
         return f.read()
+
 
 EXAMPLE_DATA = """\
 1abc2
@@ -18,15 +21,28 @@ treb7uchet
 """
 
 import re
+
 # find only digits
-DIGIT_RE = re.compile('\d')   
+DIGIT_RE = re.compile("\d")
 
 # find digits (e.g. 1,2,3 ) or digit words (e.g. one, two, three)
 # The value for normal digits is None so we can test on it later
-WORD_VALUES = {'\d': None,
-                'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 
-               'six': 6, 'seven': 7, 'eight': 8, 'nine': 9, 'zero': 0}
+WORD_VALUES = {
+    "\d": None,
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9,
+    "zero": 0,
+}
 DIGIT_WORD_RE = re.compile("|".join(f"{dw}" for dw in WORD_VALUES.keys()))
+
+
 def solve(data, part2=False):
     result = 0
     RE = DIGIT_WORD_RE if part2 else DIGIT_RE
@@ -37,23 +53,26 @@ def solve(data, part2=False):
         while m := RE.search(l, s):
             digit_words.append(m.group())
             s = m.span()[1]
-            # we want overlapping search so for longer matches we 
+            # we want overlapping search so for longer matches we
             # set the search start back one character
-            if len(digit_words[-1]) > 1: 
+            if len(digit_words[-1]) > 1:
                 s -= 1
         digits = [str(WORD_VALUES.get(dw, dw)) for dw in digit_words]
         result += int(digits[0] + digits[-1])
     return result
 
+
 def test_example():
     result = solve(EXAMPLE_DATA)
-    print(f'example: {result}')
+    print(f"example: {result}")
     assert result == 142
+
 
 def test_part1():
     result = solve(data())
-    print('Part 1:', result)
+    print("Part 1:", result)
     assert result == 55017
+
 
 EXAMPLE_DATA2 = """\
 two1nine
@@ -63,14 +82,16 @@ xtwone3four
 4nineeightseven2
 zoneight234
 7pqrstsixteen
-"""	
+"""
+
 
 def test_example2():
     result = solve(EXAMPLE_DATA2, part2=True)
-    print(f'example 2: {result}')
+    print(f"example 2: {result}")
     assert result == 281
+
 
 def test_part2():
     result = solve(data(), part2=True)
-    print('Part 2:', result)
+    print("Part 2:", result)
     assert result == 53539
