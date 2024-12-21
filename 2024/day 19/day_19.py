147912e7a1219722593bb5dcaae28c6c80772496
Pieter Kitslaar <kitslaar@gmail.com>
2024-12-21 08:36:11 +0100

    AoC Day 19 - Part 2

--- 2024/day 19/day_19.py
+++ 2024/day 19/day_19.py
@@ -28,53 +28,34 @@ def parse(data):
             designs.append(line.strip())
     return towels, designs
 
-def solve_design(towels, design, solutions=None, level=0, early_exit=True):
-    if solutions is None:
-        solutions = []
-    #these_towels = [t for t in towels if t in design]
-    if not any(design.endswith(t) for t in towels):
-        # no way to solve this design
-        return solutions
+from functools import lru_cache
+
+# the magic solution that makes the problem tractable
+@lru_cache(maxsize=None)
+def solve_design(towels, design, early_exit=True):
+    num_solutions = 0
     for towel in towels:
         if design.endswith(towel):
-            #print(level, towel, design)
             if len(design) == len(towel):
-                solutions.append([towel])
+                num_solutions += 1
             else:
-                sub_solutions = []
-                if solve_design(towels, design[:-len(towel)], sub_solutions, level=level+1, early_exit=early_exit):
-                    for s in sub_solutions:
-                        solutions.append(s + [towel])
-                        if early_exit:
-                            break
-        if early_exit and solutions:
+                num_solutions += solve_design(towels, design[:-len(towel)], early_exit=early_exit)
+        if early_exit and num_solutions:
             break # stop when first solution found
-    return solutions
+    return num_solutions
 
-from functools import reduce
+
 
 def solve(data, part2=False):
     result = 0
     towels, designs = parse(data)
     towels.sort(key=lambda x: len(x), reverse=True)
+    towels = tuple(towels)
 
-    towel_equivalents = {}
-    if part2:
-        for i, t in enumerate(towels):
-            t_solution = solve_design(towels, t, early_exit=False)
-            if t_solution:
-                towel_equivalents[t] = t_solution
-
-    num_designs = len(designs)
     for i, design in enumerate(designs):
-        solutions = solve_design(towels, design, early_exit=True)
-        print(i, design, len(solutions), 100*i/num_designs)
-        if solutions:
-            if part2:
-                num_permutations = reduce(lambda x, y: x * y, (len(towel_equivalents[t]) for t in solutions[0]))
-                result += num_permutations
-            else:
-                result += 1
+        solve_design.cache_clear() # clear cache for each design
+        num_solutions = solve_design(towels, design, early_exit=not part2)
+        result += num_solutions
     return result
 
 
@@ -99,7 +80,7 @@ def test_example2():
 def test_part2():
     result = solve(data(), part2=True)
     print("Part 2:", result)
-    assert result == -1
+    assert result == 575227823167869
 
 
 from pathlib import Path
